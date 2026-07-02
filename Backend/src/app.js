f505ffc1926bca21545a4a76111f2172ca5ee148
Ayush Rawat <rawatayushh10@gmail.com>
2026-07-02 20:06:01 +0530

    Written asyncHandler utility

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,16 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.use(express.json({ limit: '16kb' }));
-app.use(express.urlencoded({ limit: '16kb', extended: true }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
-app.use(cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
 
 export default app;
