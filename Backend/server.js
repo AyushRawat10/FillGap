d2bfac3800f8435e45b1c0aeb91e7443efb7a142
Ayush Rawat <rawatayushh10@gmail.com>
2026-07-02 17:55:33 +0530

    include cors and other middlewares in app.js file

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import app from "./src/app.js"
+
+dotenv.config({
+    path:"./.env"
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`server on running port : http://localhost:${port}`)
+})
---
+++ Backend/src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ limit: '16kb', extended: true }));
+app.use(express.static("public"));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
+
+export default app;
