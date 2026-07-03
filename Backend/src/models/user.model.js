d39872f96234e726f130b6ecd69dfaeb22a7d89c
Ayush Rawat <rawatayushh10@gmail.com>
2026-07-03 17:32:53 +0530

    feat: add user model with password hashing, JWT token generation, and temporary token functionality

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new Schema({
     username: {
@@ -19,7 +22,7 @@ const userSchema = new Schema({
     fullname: {
         type: String,
         trim: true,
-    }, 
+    },
     password: {
         type: String,
         required: [true, "Password is required"],
@@ -39,8 +42,55 @@ const userSchema = new Schema({
     },
     forgotPasswordToken: {
         type: String,
-    }, 
+    },
     forgotPasswordExpiry: {
         type: Date,
-    }
-})
+    },
+});
+
+userSchema.pre("save", async function () {
+    if (!isModified(this.password)) return;
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    );
+};
+
+userSchema.methods.generateTemporaryToken = function () {
+    const unHashedToken = crypto.randomBytes(20).toString("hex");
+
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(unHashedToken)
+        .digest("hex");
+
+    const tokenExpiry = Date.now() + 20 * 60 * 1000;
+
+    return { unHashedToken, hashedToken, tokenExpiry };
+};
+
+export const User = mongoose.model("User", userSchema);
