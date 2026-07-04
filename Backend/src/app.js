e575c783292739c4d37a213959b7f0278d8dc15f
Ayush Rawat <rawatayushh10@gmail.com>
2026-07-04 16:44:32 +0530

    add getCurrentUser endpoint to fetch current user details

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
-import authRouter from "./routes/auth.route.js"
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
+app.use(cookieParser());
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
@@ -15,6 +17,6 @@ app.use(
     })
 );
 
-app.use("api/v1/auth", authRouter)
+app.use("api/v1/auth", authRouter);
 
 export default app;
