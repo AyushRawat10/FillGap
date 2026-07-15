3dd055b10cbba7c02c1d47f5938c29ad757985c7
Ayush Rawat <rawatayushh10@gmail.com>
2026-07-15 16:52:34 +0530

    remove 'sameSite' attribute from cookie options in login, logout, and refresh token functions

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -119,7 +119,6 @@ const loginUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
-        sameSite: "none"
     };
 
     return res
@@ -151,7 +150,6 @@ const logoutUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
-        sameSite: "none"
     };
 
     return res
@@ -262,7 +260,6 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
         const options = {
             httpOnly: true,
             secure: true,
-            sameSite: "none"
         };
 
         const { accessToken, refreshToken: newRefreshToken } =
